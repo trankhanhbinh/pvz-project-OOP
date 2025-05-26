2b717c79b121b0197f4bdb1cf5cea0ee08a57cb3
Trần Khánh Bình <tranbinh09122006@gmail.com>
2025-05-26 23:39:54 +0700

    merge

--- game/Projectile.java
+++ game/Projectile.java
@@ -67,9 +67,7 @@ public class Projectile extends animatedObjects{
                 }
                 
                 
-            }
-            
-           
+            }           
         }
     }
 }
